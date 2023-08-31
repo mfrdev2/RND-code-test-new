1f39e9bcc6b69921cde75fd8f5c5d9ff393fcff0
FRabbi <mdfazlerabbi.diu@gmail.com>
2023-08-31 15:17:59 +0600

    update

--- src/main/java/com/mfrdev/test/regularExpression/StringValidation.java
+++ src/main/java/com/mfrdev/test/regularExpression/StringValidation.java
@@ -1,14 +1,15 @@
 package com.mfrdev.test.regularExpression;
 
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringValidation {
     public static void main(String[] args) {
-        String[] strings = {"ABC0", "DEF1", "1234", "AB01", "A001", "0A254","A10x14"};
+        String[] strings = {"ABC0", "DEF1", "1234", "AB01", "A001", "0A254", "1014"};
 
-       // String regex = "^(?:([A-Za-z]\\d+(?![A-Za-z]))\\d*|\\d{1,4})$";
-       // String regex = "^(?:[A-Za-z]\\d+| [A-Za-z]+\\d+ |\\d{4})$";
+        // String regex = "^(?:([A-Za-z]\\d+(?![A-Za-z]))\\d*|\\d{1,4})$";
+        // String regex = "^(?:[A-Za-z]\\d+| [A-Za-z]+\\d+ |\\d{4})$";
         String regex = "^([A-Za-z]+)(\\d+)|\\d{4}$";
         Pattern pattern = Pattern.compile(regex);
 
@@ -18,24 +19,38 @@ public class StringValidation {
                 System.out.println("--------------------");
                 System.out.println(s + " is valid");
                 System.out.println();
-                System.out.println(s +" Digit:: "+getDigitFormStr(s));
-                System.out.println(s+ " OnlyStr:: "+getStrFormDStr(s));
+                System.out.println(s + " Digit:: " + getDigitFormStr(s));
+                System.out.println(s + " OnlyStr:: " + getStrFormDStr(s));
                 System.out.println();
             } else {
                 System.out.println("-------------------------");
                 System.out.println(s + " is invalid");
-                System.out.println(s +" Digit:: "+getDigitFormStr(s));
-                System.out.println(s+ " OnlyStr:: "+getStrFormDStr(s));
+                System.out.println(s + " Digit:: " + getDigitFormStr(s));
+                System.out.println(s + " OnlyStr:: " + getStrFormDStr(s));
                 System.out.println();
             }
         }
+
+        String open = createValidMessage("Open");
+        System.out.println(open.length());
     }
 
-    public static String getDigitFormStr(String str){
-        return str.replaceAll("\\D+","");
+    public static String getDigitFormStr(String str) {
+        return str.replaceAll("\\D+", "");
     }
-    public static String getStrFormDStr(String str){
+
+    public static String getStrFormDStr(String str) {
         return str.replaceAll("[^A-Za-z]+", "");
     }
 
+    public static String createValidMessage(String msg) {
+        if (Objects.isNull(msg) || msg.isEmpty()) {
+            return "";
+        }
+
+        if (msg.length() < 8) {
+            return " ".repeat(Math.max(0, (8-msg.length()))) + msg;
+        }
+        return msg;
+    }
 }
